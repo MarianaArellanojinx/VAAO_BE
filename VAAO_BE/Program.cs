988ec0766c4ec902ec79bf07b539446011ab9a05
Gerardo3Carcamo <gcarcamo@uadec.edu.mx>
2025-12-28 13:26:10 -0600

    add cors

--- VAAO_BE/Program.cs
+++ VAAO_BE/Program.cs
@@ -6,6 +6,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("all", policy =>
+    {
+       policy.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod(); 
+    });
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -14,7 +23,7 @@ builder.Services.AddDbContext<VAAOContext>();
 builder.Services.AddScoped<IUsersRepository, UserRepository>();
 builder.Services.AddScoped<IClientesRepository, ClientesRepository>();
 var app = builder.Build();
-
+app.UseCors("all");
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
